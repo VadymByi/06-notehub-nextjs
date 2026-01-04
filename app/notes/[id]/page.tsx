29debd05c1b73c0ee7997f2c60f032b9c62c4a06
Buinov Vadym <karabincom@gmail.com>
2026-01-04 17:26:50 +0200

    removed force-dynamic

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-
 import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from '../NoteDetails.client';
 import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-
 import css from './Home.module.css';
 
 export default function Home() {
