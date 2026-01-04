3a953459286707698077818ace9b6290c5805d82
Buinov Vadym <karabincom@gmail.com>
2026-01-04 17:36:05 +0200

    moved NoteDetails.client.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { fetchNoteById } from '@/lib/api';
-import NoteDetailsClient from '../NoteDetails.client';
+import NoteDetailsClient from './NoteDetails.client';
 import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
 interface Props {
